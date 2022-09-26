99566bbe5558b0684fced62ab310ff14d4c055e5
zaldeth <113926156+zaldeth@users.noreply.github.com>
2022-09-26 20:06:02 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,97 +1,43 @@
-var b, enemyImage, ballImage, scene, ball, alien1, alien2, alien3, score=0, opscore=0, speed=5
+var sea,ship;
+var seaImg,shipImg;
 
 function preload(){
-  
-  b = loadImage("b.png");
-  enemyImage = loadImage("alien.png");
-  ballImage = loadImage("ball.png");
-  
+  seaImg = loadImage("sea.png");
+  shipImg1=loadAnimation("sea.png")
+//Choose the correct option by uncommenting the right line to load the animation for ship.
+  shipImg1 = loadAnimation("ship-1.png","ship-1.png","ship-2.png","ship-1.png");
+  //shipImg1 = loadAnimation("ship-1","ship-1","ship-2","ship-1");
+  //shipImg1 = loadAnimation("1.png","2.png","3.png","4.png");
+
 }
 
+function setup(){
+  createCanvas(400,400);
+  background("blue");
 
-
-function setup() {
-  createCanvas(400, 400);
-  
-  scene = createSprite(200,200);
-  scene.addImage(b)
-  ball = createSprite(200,345,200,345)
-  ball.addImage(ballImage)
-  ball.scale=0.1
-
-  alien1 = createSprite(200,280,10,10)
-  alien2 = createSprite(200,170,10,10)
-  alien3 = createSprite(200,50,10,10)
-  alien1.addImage(enemyImage)
-  alien2.addImage(enemyImage)
-  alien3.addImage(enemyImage)
-
-  alien1.scale=0.3
-  alien2.scale=0.3
-  alien3.scale=0.3
-
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.scale=0.3;
 
   
-   score = 0    
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
 }
 
 function draw() {
- background(0);
- createEdgeSprites()
-
- alien1.x=alien1.x+speed
- alien2.x=alien2.x-speed
- alien3.x=alien3.x+speed
-
- if(alien1.x<0 || alien1.x>width)
- {
-   speed=speed*-1
- }
-
-
-
-if(keyDown(UP_ARROW)){
-  ball.y=ball.y-3
-}
-
-if(keyDown(DOWN_ARROW)){
-  ball.y=ball.y+3
-}
-
-if(keyDown(LEFT_ARROW)){
-  ball.x=ball.x-3
-}
-
-if(keyDown(RIGHT_ARROW)){
-  ball.x=ball.x+3
-}
-
-if(ball.isTouching(alien1)|| ball.isTouching(alien2)|| ball.isTouching(alien3)){
-    ball.x=200
-  ball.y=350
-  opscore = opscore+1
-}
-if(ball.y<0){
-  ball.x=200
-  ball.y=345
-  score=score+1
-}
-
-
-drawSprites();
-textSize(15)
-  fill("Yellow")
-  text("Humans:"+score,328,30);
+  background(0);
+  
+//Uncomment the correct line to make the background move to create a forward moving effect for the boat.
+  //sea.velocityY = -3;
+  //sea.x = -3;
+  sea.velocityX = -3;
+  //sea.velocityX = 3;
   
 
-textSize(15)
-  fill("Yellow")
-  text("Aliens:"+opscore,5,30);
   
-
     
-
+  drawSprites();
 }
-
-
-
